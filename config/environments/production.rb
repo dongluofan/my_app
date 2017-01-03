e37e6ba12d6b9c37f4c53a54234e30a83b796ad4
dongluofan <2385249460@qq.com>
2017-01-03 03:18:23 -0400

    Signup try third

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ MyProject::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -60,7 +60,7 @@ MyProject::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
+  config.consider_all_requests_local = true
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
