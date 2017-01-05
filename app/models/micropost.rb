c5dd215b4eb191b7b95eb11b908f622988bc6652
dongluofan <2385249460@qq.com>
2017-01-05 10:19:38 -0400

    change length of micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,9 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content,:title
 	belongs_to :user
-	has_many :commends,:dependent => :destroy
-	validates :content, :presence =>true, :length => {:maximum => 140}
-	validates :title, :presence =>true, :length => {:maximum => 40}
+	has_many :commends, :dependent => :destroy
+	validates :content, :presence =>true, :length => {:maximum => 400}
+	validates :title, :presence =>true, :length => {:maximum => 140}
 	validates :user_id, :presence =>true
 	default_scope :order => 'microposts.created_at DESC'
 end
